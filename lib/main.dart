0e83aae5770641e4f8a799dba12fad19bce7076e
monjurur <monjurur.roshid@efgt.eurofoods-bd.com>
2024-03-04 15:26:36 +0600

    project done

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'dart:io';
+
+import 'package:capture_and_save_image_with_custom_name/pages/homePage.dart';
 import 'package:flutter/material.dart';
 import 'package:image_gallery_saver/image_gallery_saver.dart';
 import 'package:image_picker/image_picker.dart';
@@ -11,75 +13,53 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: HomePage(),
+    return  const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Page(),
     );
   }
 }
 
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+
+class Page extends StatefulWidget {
+  const Page({super.key});
 
   @override
-  State<HomePage> createState() => _HomePageState();
+  State<Page> createState() => _PageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _PageState extends State<Page> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    pageNavigator();
+  }
+
+  pageNavigator() {
+    Future.delayed(const Duration(seconds: 2), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) =>  const HomePage(),
+        ),
+      );
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () async {
-            requestStoragePermission();
-            String? imagePath = await pickAndSaveImage();
-            if (imagePath != null) {
-              saveImageToGallery(imagePath);
-            }
-          },
-          child: const Text('Capture'),
-        ),
+      body: Container(
+        color: Colors.white,
+        child: Center(child: Image.asset("assets/images/efgt.jpeg"),),
       ),
     );
   }
 
-  Future<bool> requestStoragePermission() async {
-    final status = await Permission.storage.request();
-    if (status == PermissionStatus.granted) {
-      return true;
-    } else {
-      // Handle permission denied case
-      return false;
-    }
-  }
-
-  Future<String?> pickAndSaveImage() async {
-    final pickedFile =
-        await ImagePicker().pickImage(source: ImageSource.camera);
-    if (pickedFile == null) {
-      return null;
-    }
-
-    final fileName = '${DateTime.now()}fahim.jpg'; // Generate unique name
-    final directory = await getApplicationDocumentsDirectory();
-    final newPath = '${directory.path}/$fileName';
-
-    // Read bytes from picked file
-    final bytes = await pickedFile.readAsBytes();
-
-    // Create a new file with custom name
-    final newFile = File(newPath);
-
-    // Write bytes to the new file
-    await newFile.writeAsBytes(bytes);
-
-    return newPath; // Return the path of the copied file
-  }
-
-  Future<void> saveImageToGallery(String imagePath) async {
-    await ImageGallerySaver.saveFile(imagePath, name: 'Fahim.jpg');
-  }
 }
---
+++ lib/pages/homePage.dart
@@ -0,0 +1,326 @@
+import 'dart:io';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
+import 'package:image_gallery_saver/image_gallery_saver.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'dart:developer' as d;
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String? imagePath;
+  var pickedFile;
+  bool permission = false;
+  var images;
+
+  TextEditingController idController = TextEditingController();
+  TextEditingController nameController = TextEditingController();
+  FocusNode idFocusNode = FocusNode();
+  FocusNode nameFocusNode = FocusNode();
+  final formKey = GlobalKey<FormState>();
+ /* @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    images =  Image.asset(
+      "assets/images/person.jpg",
+      fit: BoxFit.cover,
+    );
+  }*/
+
+  @override
+  Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: SingleChildScrollView(
+            child: Form(
+              key: formKey,
+              child: Column(
+                children: [
+                  Container(
+                    width: width / 2,
+                    height: height / 4,
+                    clipBehavior: Clip.antiAlias,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(
+                            15) // Adjust the radius as needed
+                        ),
+                    child: images == null
+                        ? Image.asset(
+                      "assets/images/empty_image.jpg",
+                      fit: BoxFit.cover,
+                    )
+                        : Image.file(
+                            images!,
+                            fit: BoxFit.cover,
+                          ),
+                  ),
+                  const Text(
+                    "Profile Image",
+                    style: TextStyle(fontSize: 25),
+                  ),
+                  const SizedBox(
+                    height: 40,
+                  ),
+                  Center(
+                    child: SizedBox(
+                      height: 45,
+                      width: width / 2,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          elevation: 5, // Add elevation here
+                          //padding: EdgeInsets.all(20),
+                          backgroundColor: Colors.lightBlueAccent,
+                            shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                        ),
+                        onPressed: () async {
+                          try {
+                            await requestStoragePermission();
+                            if (permission) {
+                              imagePath = await pickAndSaveImage();
+                            }
+                            setState(() {});
+                          } catch (e) {
+                            d.log("Error[Request Permission and Camera Open]:",
+                                error: e);
+                          }
+                        },
+                        child:   const Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text("Camera"),
+                            SizedBox(
+                              width: 5,
+                            ),
+                            Icon(Icons.camera_alt)
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  const Row(
+                    children: [
+                      Text(
+                        'Id',
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: Colors.black,
+                          fontWeight: FontWeight.w300,
+                        ),
+                      ),
+                      Text(
+                        '*',
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: Colors.red,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                  TextFormField(
+                    controller: idController,
+                    keyboardType: TextInputType.name,
+                    focusNode: idFocusNode,
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Please enter Id';
+                      }
+                      return null;
+                    },
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Add Id',
+                      hintStyle: TextStyle(
+                        fontSize: 14,
+                        color: Colors.grey,
+                        fontWeight: FontWeight.w300,
+                      ),
+                      //  border bottom
+                    ),
+                    onTap: () {},
+                    onEditingComplete: () {
+                      setState(() {
+                        /*firstNameFocusNode.unfocus();
+                        lastNameFocusNode.requestFocus();*/
+                      });
+                    },
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  const Row(
+                    children: [
+                      Text(
+                        'Full Name',
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: Colors.black,
+                          fontWeight: FontWeight.w300,
+                        ),
+                      ),
+                      Text(
+                        '*',
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: Colors.red,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                  TextFormField(
+                    controller: nameController,
+                    keyboardType: TextInputType.name,
+                    focusNode: nameFocusNode,
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Please enter full name';
+                      }
+                      return null;
+                    },
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Add full Name',
+                      hintStyle: TextStyle(
+                        fontSize: 14,
+                        color: Colors.grey,
+                        fontWeight: FontWeight.w300,
+                      ),
+                      //  border bottom
+                    ),
+                    onTap: () {},
+                    onEditingComplete: () {
+                      setState(() {
+                        /*firstNameFocusNode.unfocus();
+                        lastNameFocusNode.requestFocus();*/
+                      });
+                    },
+                  ),
+                  const SizedBox(
+                    height: 40,
+                  ),
+                  Center(
+                    child: SizedBox(
+                      height: 45,
+                      width: width / 2,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          elevation: 8, // Add elevation here
+                          //padding: EdgeInsets.all(20),
+                        ),
+                        onPressed: () async {
+                          if (formKey.currentState!.validate()) {
+                            if (imagePath != null) {
+                              saveImageToGallery(imagePath!, idController.text,
+                                  nameController.text);
+                            }else{
+                              ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                                backgroundColor: Colors.deepOrangeAccent,
+                                content: Text("Pick Image Please"),
+                              ));
+                            }
+                          }
+                        },
+                        child: const Text('Save Image'),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future<void> requestStoragePermission() async {
+    try {
+      final status = await Permission.storage.request();
+      if (status == PermissionStatus.granted) {
+        permission = true;
+      } else {
+        // Handle permission denied case
+        permission = false;
+      }
+    } catch (e) {
+      d.log("error: [Request Camera Permission ]: $e");
+    }
+  }
+
+  Future<String?> pickAndSaveImage() async {
+    try {
+      pickedFile = (await ImagePicker().pickImage(source: ImageSource.camera));
+      if (pickedFile == null) {
+        return null;
+      } else {
+        images = File(pickedFile.path);
+      }
+
+      final fileName = '${DateTime.now()}.jpg'; // Generate unique name
+      final directory = await getApplicationDocumentsDirectory();
+      final newPath = '${directory.path}/$fileName';
+
+      // Read bytes from picked file
+      final bytes = await pickedFile.readAsBytes();
+
+      // Create a new file with custom name
+      final newFile = File(newPath);
+
+      // Write bytes to the new file
+      await newFile.writeAsBytes(bytes);
+
+      return newPath; // Return the path of the copied file
+    } catch (e) {
+      d.log("Error: [Camera Open And Image Pick] :$e");
+    }
+  }
+  Future<File> getImageFileFromAssets(String path) async {
+    final byteData = await rootBundle.load('$path');
+
+    final file = File('${(await getTemporaryDirectory()).path}/$path');
+    await file.create(recursive: true);
+    await file.writeAsBytes(byteData.buffer.asUint8List(byteData.offsetInBytes, byteData.lengthInBytes));
+
+    return file;
+  }
+
+  Future<void> saveImageToGallery(
+      String imagePath, String id, String name) async {
+
+      await ImageGallerySaver.saveFile(imagePath, name: '$name-$id');
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+        backgroundColor: Colors.indigoAccent,
+        content: Center(child: Text("Save Successfully")),
+      ));
+      setState(()async {
+        idController.clear();
+        nameController.clear();
+        imagePath = '';
+        images = null;
+        idFocusNode.unfocus();
+        nameFocusNode.unfocus();
+        setState(() {
+        });
+      });
+  }
+}
